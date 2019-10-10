ae8a6ea0d25fd8632ed10a3881d79024f7dd79c0
reeved <dcunhareeve@gmail.com>
2019-10-11 12:45:10 +1300

    Added initial GPIO setup code

---
+++ EE209/src/lib/gpio.c
@@ -0,0 +1,18 @@
+/*
+ * gpio.c
+ *
+ *
+ *  Author: Reeve D'Cunha
+ */ 
+
+#include "gpio.h"
+
+void gpio_init()
+{
+
+//Set all PORTC pins as inputs
+DDRC = 0x00;
+
+//Set PORTD Pins 1 and 0 as outputs
+DDRD = 0x03;
+}
---
+++ EE209/src/lib/gpio.h
@@ -0,0 +1,18 @@
+/*
+ * gpio.h
+ *
+ *
+ *  Author: Reeve D'Cunha
+ */ 
+
+#include "config.h"
+#include <avr/io.h>
+
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+void gpio_init();
+
+
+#endif
